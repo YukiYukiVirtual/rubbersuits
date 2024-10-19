2149c63e8dfa53c1b1a9d9bda21df27860b6f42d
YukiYukiVirtual <yukiyukivirtual>
2024-10-19 15:09:35 +0900

    a

--- trust/list.js
+++ trust/list.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function()
+window.addEventListener("DOMContentLoaded", function()
 {
 	function readcsv(filePath)
 	{
@@ -10,9 +10,20 @@ window.addEventListener("load", function()
 		var LF = String.fromCharCode(10); //改行ｺｰﾄﾞ
 		var lines = data.responseText.split(LF);
 		for (var i = 0; i < lines.length;++i) {
-			var cells = lines[i].split(",");
-			if( cells.length != 1 ) {
-				csvData.push(cells);
+			var cells = lines[i].replace("\r", "").split(",");
+			if( cells.length == 4 ) {
+				var tags = cells[3].split(";");
+				for(var j = 0; j < tags.length; j++)
+				{
+					tags[j] = tags[j].trim();
+				}
+				var cellData = {
+					"name": cells[0].trim(),
+					"url": cells[1].trim(),
+					"description": cells[2].trim(),
+					"tags": tags
+				};
+				csvData.push(cellData);
 			}
 		}
 		return csvData;
